2dc14ae3fbb7c676f157994d331c89efbba1e9b8
nikhil <nikhilgupta7104@gmail.com>
2024-03-05 20:52:19 +0530

    commit

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,58 +1,58 @@
-// import { IProduct, Product } from "@/lib/model/product.model";
-// import { connecttoDB } from "@/lib/mongoose";
-// import amazonpagescraper from "@/lib/scraper/amazonpagescraper";
-// import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/util";
+import { IProduct, Product } from "@/lib/model/product.model";
+import { connecttoDB } from "@/lib/mongoose";
+import amazonpagescraper from "@/lib/scraper/amazonpagescraper";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/util";
 
-// export async function GET() {
-//     try {
-//         connecttoDB()
+export default async function GET() {
+    // try {
+    //     connecttoDB()
 
-//         const product = await Product.find({})
+    //     const product = await Product.find({})
 
-//         if (!product) {
-//             throw new Error('no product found')
-//         }
+    //     if (!product) {
+    //         throw new Error('no product found')
+    //     }
 
-//         const updateProduct = await Promise.all(product.map(async (currentProduct) => {
-//             const scrapProduct = await amazonpagescraper(currentProduct.link)
+    //     const updateProduct = await Promise.all(product.map(async (currentProduct) => {
+    //         const scrapProduct = await amazonpagescraper(currentProduct.link)
 
-//             if (!scrapProduct) {
-//                 throw new Error('no product found')
-//             }
+    //         if (!scrapProduct) {
+    //             throw new Error('no product found')
+    //         }
 
-//             const updatedpricehistory = [
-//                 ...currentProduct.pricehistory,
-//                 { price: scrapProduct.price }
-//             ]
+    //         const updatedpricehistory = [
+    //             ...currentProduct.pricehistory,
+    //             { price: scrapProduct.price }
+    //         ]
 
-//             const product = {
-//                 ...scrapProduct,
-//                 pricehistory: updatedpricehistory,
-//                 low: getLowestPrice(updatedpricehistory),
-//                 high: getHighestPrice(updatedpricehistory),
-//                 avg: getAveragePrice(updatedpricehistory)
-//             }
+    //         const product = {
+    //             ...scrapProduct,
+    //             pricehistory: updatedpricehistory,
+    //             low: getLowestPrice(updatedpricehistory),
+    //             high: getHighestPrice(updatedpricehistory),
+    //             avg: getAveragePrice(updatedpricehistory)
+    //         }
 
-//             const updatedProduct = await IProduct.findOneAndUpdate({ link: scrapProduct.link },
-//                 product
-//             )
+    //         const updatedProduct = await IProduct.findOneAndUpdate({ link: scrapProduct.link },
+    //             product
+    //         )
 
-//             const emailNotifType = getEmailNotifType(
-//                 scrapProduct, currentProduct
-//             )
+    //         const emailNotifType = getEmailNotifType(
+    //             scrapProduct, currentProduct
+    //         )
                 
-//             if(emailNotifType && updatedProduct.users.length > 0)
-//             {
-//                 return
+    //         if(emailNotifType && updatedProduct.users.length > 0)
+    //         {
+    //             return
                 
-//             }
+    //         }
 
-//             return 
+    //         return 
 
-//         }))
+    //     }))
 
-//     } catch (error) {
-//         console.log('error in api', error);
+    // } catch (error) {
+    //     console.log('error in api', error);
 
-//     }
-// }
+    // }
+}
