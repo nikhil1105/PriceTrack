69d7efe8094d4d4bac4789b64ff966e5bf7d3de1
nikhil <nikhilgupta7104@gmail.com>
2024-03-08 13:47:24 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className='sm:px-[30px] xl:px-36'>
+      <body className='px-[30px] xl:px-36'>
         
         <main className="max-w-10xl mx-auto">
           
