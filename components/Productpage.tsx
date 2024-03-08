af9666180fe7cd05b63051de8c2f4c216a5d141b
nikhil <nikhilgupta7104@gmail.com>
2024-03-08 16:20:52 +0530

    commit

--- components/Productpage.tsx
+++ components/Productpage.tsx
@@ -93,7 +93,7 @@ const Productpage = ({ data }: { data: any }) => {
                                     width={22}
 
                                 />
-                                <Link href={product?.link} className="text-base text-white" >Buy Now</Link>
+                                <Link href={product?.link} target="_blank" className="text-base text-white" >Buy Now</Link>
                             </button>
                         </div>
                     </div>
