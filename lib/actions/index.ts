b11f3870fccec04ca8af672d84f5bd2c3554b5e0
nikhil <149315856+nikhil1105@users.noreply.github.com>
2024-04-05 02:26:20 +0530

    commit

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -16,12 +16,13 @@ export default async function Scrapeandstore(url: string) {
 
    try {
       
-      const data1 = await amazonsrchscraper(url);
-      //const data2 = await flipkartsrchscraper(url)
-
+      const [data1, data2] = await Promise.all([
+         amazonsrchscraper(url),
+          flipkartsrchscraper(url)
+       ]);
       
 
-      const data = data1//?.concat(data2)
+      const data = data1?.concat(data2)
 
       if (!data) {
          console.log('data not found');
